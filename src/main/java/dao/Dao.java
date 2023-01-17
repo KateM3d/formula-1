61c69d42731b9cdfe2ef7620536aae95921b9eda
KateM3d <ekaterina8medvedeva@gmail.com>
2023-01-16 22:12:49 -0500

    fix Dao

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -4,7 +4,6 @@ import main.java.models.RacerModel;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -12,21 +11,30 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Dao {
-    ClassLoader classLoader = Dao.class.getClassLoader();
+    ClassLoader
+            classLoader =
+            Dao.class.getClassLoader();
 
     private BufferedReader getBufferedReader(String resource) {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(classLoader.getResourceAsStream(resource)), StandardCharsets.UTF_8));
+        BufferedReader
+                bufferedReader =
+                new BufferedReader(new InputStreamReader(Objects.requireNonNull(classLoader.getResourceAsStream(resource)),
+                        StandardCharsets.UTF_8));
         return bufferedReader;
     }
 
     public List<RacerModel> getListOfRacers() {
-        List<RacerModel> racers;
+        List<RacerModel>
+                racers;
 
         try (BufferedReader bufferedReader = getBufferedReader("abbreviations.txt")) {
-            racers = bufferedReader.lines()
-                    .map(line -> line.split("_"))
-                    .map(str -> new RacerModel(str[0], str[1], str[2]))
-                    .collect(Collectors.toList());
+            racers =
+                    bufferedReader.lines()
+                            .map(line -> line.split("_"))
+                            .map(str -> new RacerModel(str[0],
+                                    str[1],
+                                    str[2]))
+                            .collect(Collectors.toList());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
